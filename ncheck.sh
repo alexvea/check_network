0259b946796b87aca57b47ceaa9a30724dcae321
alexvea <35368807+alexvea@users.noreply.github.com>
2024-04-17 15:59:24 +0200

    Update ncheck.sh
    removed function

--- ncheck.sh
+++ ncheck.sh
@@ -16,13 +16,14 @@ Help()
    echo "-p     port"
    echo
 }
+set -e
 DESTINATION=""
 SUDO_CMD="/usr/bin/env sudo -u ssh_user"
 SSH_CMD=""
 NETSTAT_CMD="/usr/bin/env netstat"
-IP_CMD="/usr/bin/env ip"
+IP_CMD="/usr/bin/env ip a"
 AWK_CMD="/usr/bin/env awk"
-PING_CMD="/usr/bin/env ping"
+PING_CMD="/usr/bin/env ping -c 1 -W 1"
 NC_CMD="/usr/bin/env nc"
 
 while getopts "hdH:D:P:p:" option; do
@@ -53,37 +54,25 @@ done
 
 function check_if_host_is_local {
         [[ $1 =~ ^(127.0.0.1|localhost) ]] && return 0
-        OUTPUT=$($IP_CMD a | grep $1)
+        OUTPUT=$($IP_CMD | grep $1)
         [[ ! -z $OUTPUT ]] && return 0 || return 1
-}
-
-function check_network_flow {
-        [[ ! -z $1 ]] && return 0 || return 1
-}
-
-function check_listen_port {
-        [[ "$2" =~ ^(ICMP|icmp)$ ]] && return 0
-        F_SSH_CMD="$SUDO_CMD ssh ssh_user@$1"
-        #$SSH_CMD $NETSTAT_CMD -tulan | grep $PROTOCOL | egrep "(0.0.0.0:|:::|$HOST:)" | egrep ":$PORT\s" | grep LISTEN
-        $F_SSH_CMD $NETSTAT_CMD -tulan | grep $2 | egrep "(0.0.0.0:|:::|$1:)" | egrep ":$3\s" && return 0 || return 1
 }
 
 function create_listen_port {
         F_SSH_CMD="$SUDO_CMD ssh ssh_user@$1"
-        [[ $DESTINATION == "$(localhost_to_primary_ip)" ]] && F_SSH_CMD="$SUDO_CMD"
         F_PROTOCOL=$2
         F_PORT=$3
+        [[ $DESTINATION == "$(localhost_to_primary_ip)" ]] && F_SSH_CMD="$SUDO_CMD"
+        [[ "$F_PROTOCOL" =~ ^(ICMP|icmp)$ ]] && DST_FILTER="$F_PROTOCOL" || DST_FILTER="dst port $F_PORT and $F_PROTOCOL"
         sleep 1 && $SSH_CMD $CHECK_CMD $DESTINATION $PORT 2> /dev/null &
-        #result=$($F_SSH_CMD "sudo tcpdump -c 1 -n "src host $HOST and dst port $F_PORT and $F_PROTOCOL" 2> /dev/null && echo ok")
-        result=$($F_SSH_CMD sudo tcpdump -c 1 -n src host $HOST and dst port $F_PORT and $F_PROTOCOL 2> /dev/null && echo ok)
+        result=$($F_SSH_CMD sudo tcpdump -c 1 -n src host $HOST and $DST_FILTER 2> /dev/null && echo ok)
         echo $result
-#       $F_SSH_CMD "sudo tcpdump -c 1 -n "src host $HOST and dst port $F_PORT and $F_PROTOCOL" && echo ok" & 
-#       sleep 1 && $SSH_CMD $CHECK_CMD $DESTINATION $PORT
-
 }
+
 function localhost_to_primary_ip {
         echo $(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 }
+
 if check_if_host_is_local $HOST; then
         HOST_IS_LOCAL=1
         HOST=$(localhost_to_primary_ip)
@@ -92,14 +81,13 @@ else
         SSH_CMD="$SUDO_CMD ssh ssh_user@$HOST"
 fi
 
-
 #cas check network flow from X to Y
-if check_network_flow $DESTINATION ; then
+if [[ ! -z $DESTINATION ]] ; then
         case $PROTOCOL in 
                 ICMP|icmp)
                 #remote 10.25.12.240>10.25.15.133 icmp 
                 #sudo -u ssh_user ssh 'ssh_user@10.25.12.240' "ping -c 1 10.25.15.133 &> /dev/null"
-                CHECK_CMD="$PING_CMD -c 1"
+                CHECK_CMD="$PING_CMD"
                 PORT=""
                 ;;
                 TCP|tcp)
@@ -113,11 +101,10 @@ if check_network_flow $DESTINATION ; then
                 CHECK_CMD="$NC_CMD -vz -u"
                 ;;
         esac
-        #check_listen_port $DESTINATION $PROTOCOL $PORT && $SSH_CMD $CHECK_CMD $DESTINATION $PORT || create_listen_port $DESTINATION $PROTOCOL $PORT
         $SSH_CMD $CHECK_CMD $DESTINATION $PORT || create_listen_port $DESTINATION $PROTOCOL $PORT
-        else
+else
         #cas check port remote
         #cas tcp/udp sudo -u ssh_user ssh ssh_user@10.25.12.240 /usr/bin/env netstat -tulan | awk  '$1 ~ "udp" && $4 ~ /(0.0.0.0:68|:::68|10.25.12.240:68)/ && $5 ~ /(0.0.0.0:*|:::*)/'
-        [[ "$PROTOCOL" =~ ^(ICMP|icmp)$ ]] && $SSH_CMD $PING_CMD -c 1 $HOST || $SSH_CMD $NETSTAT_CMD -tulan | $AWK_CMD -v proto="$PROTOCOL" -v host="$HOST" -v port="$PORT"$ '$1 ~ proto && $4 ~ ("0.0.0.0:"port"|:::"port"|"host":"port) && $5 ~ ("0.0.0.0:*|:::*")' 
+        [[ "$PROTOCOL" =~ ^(ICMP|icmp)$ ]] && $SSH_CMD $PING_CMD $HOST || $SSH_CMD $NETSTAT_CMD -tulan | $AWK_CMD -v proto="$PROTOCOL" -v host="$HOST" -v port="$PORT"$ '$1 ~ proto && $4 ~ ("0.0.0.0:"port"|:::"port"|"host":"port) && $5 ~ ("0.0.0.0:*|:::*")' 
 
 fi
